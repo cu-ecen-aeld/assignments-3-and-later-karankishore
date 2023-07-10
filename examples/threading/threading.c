4280559cc82369049673892fd7c901ff2b35efd0
Karan Kishore <karan.kishore@netradyne.com>
2023-07-11 01:06:35 +0530

    Assignment 4 Part 1 Solution

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,6 +14,12 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data*)thread_param;
+    usleep(thread_func_args->wait_to_obtain_ms * 1000);
+    pthread_mutex_lock(thread_func_args->mutex);
+    usleep(thread_func_args->wait_to_release_ms * 1000);
+    pthread_mutex_unlock(thread_func_args->mutex);
+    thread_func_args->thread_complete_success = true;
     return thread_param;
 }
 
@@ -28,6 +34,19 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
+    struct thread_data *data = malloc(sizeof(struct thread_data));
+    data->mutex = mutex;
+    data->wait_to_obtain_ms = wait_to_obtain_ms;
+    data->wait_to_release_ms = wait_to_release_ms;
+    data->thread_complete_success = false;
+    // start a thread using pthread_create, use threadfunc as the entry point
+    // The number of threads active should be limited only by the amount of available memory.
+    // The thread started should return a pointer to the thread_data structure when it exits, which can be used
+    // to free memory as well as to check thread_complete_success for successful exit.
+    if(pthread_create(thread, NULL, threadfunc, data) == 0)
+    {
+        return true;
+    }
     return false;
 }
 
